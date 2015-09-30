609557b407c8927dcb2985117c9cee3948251b66
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2015-09-30 14:07:00 -0500

    Terminación de parte normal, se inicia la responsiva

--- js/load.js
+++ js/load.js
@@ -19,3 +19,61 @@ $( document ).ready(function() {
 		$('.informacion[el="' + $(this).attr("el") + '"]').hide();
 	});
 });
+
+//Galeria
+
+$( document ).ready(function() {
+	var fotos = new Array();
+	var indiceImg = 0;
+	$(".elementos .foto").each(function(){
+		fotos.push($(this).attr("src"));
+	});
+	
+	$(".plus").click(function(){
+		indice = $(this).attr("img") - 1;
+		
+		setData();
+	});
+	
+	$("#modal .izq").click(function(){
+		indice--;
+		
+		indice = indice < 0?(fotos.length - 1):indice;
+		setData();
+	});
+	
+	$("#modal .der").click(function(){
+		indice++;
+		
+		indice = indice >= fotos.length?0:indice;
+		setData();
+	});
+	
+	function setData(){
+		$(".contenidoVisual").html('<img class="imagenModal" src="' + fotos[indice] + '" />');
+		$("#modal .contenido .datos h1").html(imgGaleria[indice]['titulo']);
+		$("#modal .contenido .datos p").html(imgGaleria[indice]['descripcion']);
+	}
+});
+
+//Oficinas
+$( document ).ready(function() {
+	$(".oficinas .texto").hide();
+	$(".oficinas .empresa").click(function(){
+		var empresas = $(".empresas");
+		var pos = $(this).position();
+		var texto = $(".texto");
+		
+		//texto.height($(this).height());
+		y = pos.left;
+		texto.removeClass("flecha2");
+		if (pos.left + $(this).width() + 10 > empresas.width()){
+			y -= $(this).width();
+			texto.addClass("flecha2");
+		}else
+			y += $(this).width();
+		
+		texto.css({top: pos.top, left: y, position:'absolute'});
+		$(".oficinas .texto").show();
+	});
+});
